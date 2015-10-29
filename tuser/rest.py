75150fc656f6f13a8442c8cf8b7689200b476f2d
wjx <wjx@gzqichang.com>
2015-10-29 21:25:17 +0800

    validate userinfo

--- tuser/rest.py
+++ tuser/rest.py
@@ -12,6 +12,10 @@ from . import models
 from .models import Tuser
 
 import functools
+import re
+
+USERNAMEVALIDIATE = r'([A-Za-z][\w]{5,29})'
+PASSWORDVALIDIATE = r'([\S]{6,30})'
 
 def login_required(f):
     @functools.wraps(f)
@@ -35,26 +39,34 @@ class UserCreateuserializers(serializers.ModelSerializer):
         model = models.Tuser
         fields = ('username', 'email', 'password',)
 
+    def validate_username(self, value):
+        if value:
+            if not re.match(USERNAMEVALIDIATE, value):
+                raise ValidationError('用户名必须6-30位,只能包含数字字母下划线,并以字母开头')
+        return value
+
+    def validate_email(self, value):
+        if value:
+            if Tuser.objects.filter(email=value).exists():
+                    raise ValidationError('邮箱存在')
+        return value
+
+    def validate_password(self, value):
+        if value:
+            if not re.match(PASSWORDVALIDIATE, value):
+                raise ValidationError('密码为6-30位，且为非空字符')
+        return value
+
     def create(self, validated_data):
-        # 密码处理
         validated_data['password'] = make_password(validated_data['password'])
         return super(UserCreateuserializers, self).create(validated_data)
 
-class Loginserializers(serializers.Serializer):
-
-    username = serializers.CharField(label='帐号')
-    password = serializers.CharField(label='密码')
-
-    def get_user(self):
-        return authenticate(username='hello', password='123456')
-
-    user = None
 
 class UserinfoGetserializers(serializers.ModelSerializer):
 
     class Meta:
         model = models.Tuser
-        fields = ('pk', 'email', 'avatar', 'sex',
+        fields = ('pk', 'username', 'email', 'avatar', 'sex', 'nickname',
                   'country', 'province', 'city', 'birthday',
                   'followers_count', 'grade')
 
@@ -62,8 +74,9 @@ class UserinfoCreateserializers(serializers.ModelSerializer):
 
     class Meta:
         model = models.Tuser
-        fields = ('email', 'avatar', 'sex',
+        fields = ('email', 'avatar', 'sex', 'nickname',
                   'country', 'province', 'city', 'birthday',)
+
 class CreateuserView(generics.CreateAPIView):
     serializer_class = UserCreateuserializers
 
